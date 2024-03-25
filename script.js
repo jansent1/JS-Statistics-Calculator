0813f1a3d6016f214e9b9856f5a48603ca58a2f6
Teun Jansen <teunjansen91@gmail.com>
2024-03-25 17:15:40 +0100

    getMean() and the form refresh bug

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Statistics is a way of using math to make sense of data.
 // The Javascript below will calculate the Mean, Median, Mode, Range, Variance and Standard Deviation of the given data.
 
+// Content and Calculation for the first paragraph, MEAN:
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+/*{
+  // Condense the array in a single value (for each el in the array the accumulator((initial value is 0 as second parameter of the callback function)) will be added to the sum variable):
+  const sum = array.reduce((acc, el) => {
+    return acc + el, 0;
+  });
+  const mean = sum / array.length;
+  // console.log(mean);
+  return mean;
+};*/
+
 // The calculate function called when the form is submitted:
 const calculate = () => {
   // find the numbers inside the inputfield:
@@ -13,4 +26,8 @@ const calculate = () => {
       Number(el);
     })
     .filter((el) => !isNaN(el)); // Filter out the Not a Number (NaN) input values.
+  // console.log(array);
+  const mean = getMean(numbers);
+  // Display the value of meaan inside the #mean HTML5 Element:
+  document.querySelector("#mean").textContent = mean;
 };
